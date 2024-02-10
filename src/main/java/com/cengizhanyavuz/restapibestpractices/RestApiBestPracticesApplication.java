bff26146cce7be283901ee3de72a3a9ac893ec43
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-02-10 10:15:02 +0300

    first commit

---
+++ src/main/java/com/cengizhanyavuz/restapibestpractices/RestApiBestPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.cengizhanyavuz.restapibestpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiBestPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiBestPracticesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cengizhanyavuz/restapibestpractices/RestApiBestPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cengizhanyavuz.restapibestpractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBestPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
